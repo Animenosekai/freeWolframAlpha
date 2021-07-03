3ae85baa8271a214b97713160b5527a3d2c2d902
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2021-07-04 01:08:21 +0200

    [fix] adjust pod height when a skeleton is created

--- dev/skeleton.js
+++ dev/skeleton.js
@@ -47,4 +47,5 @@ async function createSkeleton() {
 
     document.getElementById("pod").innerHTML = ""
     document.getElementById("pod").appendChild(newElem)
+    document.getElementById("pod-height").style.height = document.getElementById("pod").offsetHeight + 50 + "px"
 }
